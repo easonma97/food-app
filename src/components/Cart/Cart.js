1297c9b4ae8002396e42f6e797bd0d8001308401
Eason Ma <a705193483@gmail.com>
2023-03-22 00:10:25 +1030

    -02 commit

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -16,7 +16,14 @@ const Cart = props => {
 
     const cartItems = <ul className={classes['cart-items']}>
     {cartCtx.items.map(item => (
-        <CartItem key={item.id} name={item.name} amount={item.amount} price={item.price} onRemove={cartItemRemoveHandler.bind(null, item.id)} onAdd={cartItemAddHandler.bind(null, item)}/>
+        <CartItem 
+        key={item.id} 
+        name={item.name} 
+        amount={item.amount} 
+        price={item.price} 
+        onRemove={cartItemRemoveHandler.bind(null, item.id)} 
+        onAdd={cartItemAddHandler.bind(null, item)}
+        />
     ))}
     </ul>;
 
--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -8,9 +8,21 @@ const defaultCartState = {
 
 const cartReducer = (state, action) => {
     if (action.type === 'ADD'){
-        // return a new array instead of editing the original array
-        const updatedItems = state.items.concat(action.item);
         const updatedTotalAmount = state.totalAmount + action.item.price * action.item.amount;
+        const existingCartItemIndex = state.items.findIndex((item) => item.id === action.item.id);
+        const existingCartItem = state.items[existingCartItemIndex];
+        let updatedItems;
+        if(existingCartItem){
+            const updatedItem = {
+                ...existingCartItem,
+                amount: existingCartItem.amount + action.item.amount
+            };
+            updatedItems = [...state.items];
+            updatedItems[existingCartItemIndex] = updatedItem;
+        }else{
+            // return a new array instead of editing the original array
+            updatedItems = state.items.concat(action.item); 
+        }
         return {
             items: updatedItems,
             totalAmount: updatedTotalAmount,
