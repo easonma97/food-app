11606f30d47b8dbf67acf4a70a41446bb32e95a7
Eason Ma <a705193483@gmail.com>
2023-03-21 11:20:30 +1030

    Addding a Cart Context

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,3 +1,4 @@
+import Modal from '../UI/Modal';
 import classes from './Cart.module.css';
 
 const Cart = props => {
@@ -6,17 +7,17 @@ const Cart = props => {
     <li>{item.name}</li>)}
     </ul>;
 
-    return <div>
+    return <Modal onClose={props.onClose}>
         {cartItems}
         <div className={classes.total}>
             <span>Total Amount</span>
             <span>35.61</span>
         </div>
         <div className={classes.actions}>
-            <button className={classes['button--alt']}>Close</button>
+            <button className={classes['button--alt']} onClick={props.onClose}>Close</button>
             <button className={classes.button}>Order</button>
         </div>
-    </div>
+    </Modal>
 };
 
 export default Cart;
